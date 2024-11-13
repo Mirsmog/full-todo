7837a7a30a88ceb6f3a58fe95ff0c63fbf753f4a
zwine <tanharka.tyuchiev4@gmail.com>
2024-11-13 10:30:45 +0500

    init

--- src/routers/todoRouter.ts
+++
@@ -1,6 +0,0 @@
-import Router, { Request, Response } from 'express';
-
-const router = Router();
-router.get('/', (req: Request, res: Response) => {
-  res.json(`Welcome - Routers: ${req.headers.host}`);
-});
--- src/routers/welcomeRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-export default express
-  .Router()
-  .get('/', (req: express.Request, res: express.Response) =>
-    res.json({
-      intro: 'Welcome to the Todo api 🎉',
-      routes: ['/todo', '/todo/:id'],
-    })
-  );
